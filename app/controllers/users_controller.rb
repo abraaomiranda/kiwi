7906734502f6fc7f9d2a2da0a791c13569f21948
Abraão Miranda <abraaomirandar@gmail.com>
2017-02-25 16:03:47 -0300

    List of follows/followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, except: :index
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find params[:id]
     @posts = @user.posts.includes(:author)
   end
+
+  def follows
+    @users = @user.follows
+  end
+
+  def followers
+    @users = @user.followers
+  end
+
+  private
+
+  def set_user
+    @user = User.find params[:id]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    member do
+      get :follows
+      get :followers
+    end
+  end
+
   resources :posts, only: [:create, :destroy]
   resources :follows, only: [:create, :destroy]
   resources :home, only: :index
