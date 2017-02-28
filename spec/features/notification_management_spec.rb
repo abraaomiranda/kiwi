bde09cad44f018313535a702431c06c6c172c0f3
Abraão Miranda <abraaomirandar@gmail.com>
2017-02-28 02:05:27 -0300

    Refactoring notifications management tests

--- spec/features/notification_management_spec.rb
+++ spec/features/notification_management_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature 'Notification Management', type: :feature do
-  given(:user) { create :user }
   given(:other_user) { create :user, name: 'George Harrison' }
   given!(:notification) { create :notification, owner: other_user }
 
@@ -12,17 +11,32 @@ RSpec.feature 'Notification Management', type: :feature do
     expect(page).to have_text("#{other_user.name} #{notification.message}")
   end
 
-  scenario 'A notification should be created when a user is followed' do
-    another_user = create :user
-    login_as(user)
-    visit user_path(another_user)
+  scenario 'Logged in user should see his own notifications' do
+    login_as notification.recipient
+    visit root_path
 
-    expect{ click_button 'Seguir' }.to change{Notification.count}.by(1)
+    within('#notificationList') do
+      expect(page).to have_text("#{other_user.name} #{notification.message}")
+    end
   end
 
-  scenario 'Logged in user should see his own notifications' do
-    notified_user = notification.recipient
-    login_as notified_user
+  scenario 'Logged in user should not see notifications for another user' do
+    login_as create :user
+    visit notifications_path
+
+    expect(page).to_not have_text("#{other_user.name} #{notification.message}")
+  end
+
+  scenario 'logged in user should see number of not read notifications on navigation bar' do
+    create :notification, recipient: notification.recipient, read: true
+    login_as notification.recipient
+    visit root_path
+
+    expect(find('#notificationCounter')).to have_text('1')
+  end
+
+  scenario 'Logged in user should list its own notifications' do
+    login_as notification.recipient
     visit root_path
 
     within('#notificationList') do
@@ -31,19 +45,5 @@ RSpec.feature 'Notification Management', type: :feature do
 
     expect(page).to have_current_path(notifications_path)
     expect(page).to have_text("#{other_user.name} #{notification.message}")
-  end
-
-  scenario 'Logged in user should not see notifications for another user' do
-    login_as user
-    visit notifications_path
-
-    expect(page).to_not have_text("#{other_user.name} #{notification.message}")
-  end
-
-  scenario 'logged in user should see number of not read notifications on navigation bar' do
-    login_as notification.recipient
-    visit root_path
-
-    expect(find('#notificationCounter')).to have_text('1')
   end
 end
